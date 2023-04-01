3f712e762abdcd8d20f4693621eea16c5d2de648
Antoine <aaltorffer@orange.fr>
2023-04-01 12:14:14 +0200

    Basic UI setup.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    use eframe::egui::Visuals;
+
+    eframe::run_native(
+        "Egui node graph example",
+        eframe::NativeOptions::default(),
+        Box::new(|cc| {
+            cc.egui_ctx.set_visuals(Visuals::dark());
+            Box::new(protos_rs::ProtosApp::new(cc))
+        }),
+    );
+}
